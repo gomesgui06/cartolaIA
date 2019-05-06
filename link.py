bb65305d6b240381cb35e00ad4a8ac8da7cb4bc9
Guilherme Gomes <gg.gomesguilherme@gmail.com>
2019-05-06 08:28:51 -0300

    Adding a file with link of scrapping

---
+++ link.py
@@ -0,0 +1,3 @@
+# https://swethatanamala.github.io/2018/09/01/web-scraping-using-python-selenium-and-beautiful-soup/
+
+# http://www.compjour.org/warmups/govt-text-releases/intro-to-bs4-lxml-parsing-wh-press-briefings/
